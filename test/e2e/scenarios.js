564858758dc416a2521a36ce4b45cc776052aa67
Saem Ghani <saemghani@gmail.com>
2013-05-14 18:40:18 -0700

    Bring back the tweet length count

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -16,12 +16,21 @@ describe('my app', function() {
     it('should have a disabled tweet button', function() {
       expect(element('#tweet-button').attr('disabled')).toBe('disabled');
     });
+    
+    it('should hide the character count if nothing has been typed', function() {
+      expect(element('#tweet-length').css('display')).toBe('none');
+    });
   });
   
   describe('when there is a tweet typed in', function() {
     it('enabled the tweet button', function() {
       input('tweet').enter('more food for the sound byte culture');
       expect(element('#tweet-button').attr('disabled')).toBe(undefined);
+    });
+    
+    it('should show the correct character count', function() {
+      expect(element('#tweet-length').css('display')).toBe(undefined);
+      expect(element('#tweet-length').text()).toBe('Character(s): 36');
     });
   });
   

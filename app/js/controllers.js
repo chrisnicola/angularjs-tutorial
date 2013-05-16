195b725b05401a6c114d4a0d27498ca136a90083
Chris Nicola <chnicola@gmail.com>
2013-05-16 14:10:03 -0700

    Services and DI - Start

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,11 +3,17 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-.controller('MainController', function($scope) {
+.controller('MainController', function($scope, twitter) {
 	$scope.tweets = []
 
 	$scope.postTweet = function() {
 		$scope.tweets.push($scope.tweet)
 		$scope.tweet = ''
+	}
+
+	$scope.search = function() {
+		// use twitter.search($scope.query)
+		
+		$scope.tweets = twitter.search($scope.query)
 	}
 });

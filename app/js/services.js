51000c03772d84b9ee73bc91569f922e971b36a8
Chris Nicola <chnicola@gmail.com>
2013-05-16 14:10:33 -0700

    Services and DI - Solution

--- app/js/services.js
+++ app/js/services.js
@@ -5,4 +5,35 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []);
+angular.module('myApp.services', [])
+/* 
+	Twitter API for Searching:
+  https://dev.twitter.com/docs/api/1/get/search  
+  hint: you need to use JSONP
+
+
+  AnguarJS $http API:
+  http://docs.angularjs.org/api/ng.$http	
+  hint: add callback=JSONP_CALLBACK for AngularJS jsonp support
+*/
+.service('twitter', function TwitterService($http) {
+	var searchEndpoint = "http://search.twitter.com/search.json?callback=JSON_CALLBACK";
+	this.search = function(query) {
+		// search twitter here
+		var tweets = [];
+		$http({method: 'JSONP', url: searchEndpoint, params: {q:query}}).
+		success(function(data, status, headers, config) {
+			for (var i in data['results']) {
+        status = data['results'][i];
+				var tweet = {};
+				tweet.userName = status['from_user'];
+				tweet.text = status['text'];
+				tweet.timestamp = status['created_at'];
+        tweet.userAvatarUrl = status['profile_image_url'];
+				tweets.push(tweet);
+        console.log(status)
+			}
+		});
+		return tweets;
+	};
+});
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -4,4 +4,8 @@
 
 describe('service', function() {
   beforeEach(module('myApp.services'));
+
+  describe('when searching twitter', function() {
+  	it('should send the search query to twitter using $http')	
+  })
 });

0ffc495819fc5f59154de3b8df0c8910397d2717
Chris Nicola <chnicola@gmail.com>
2013-05-16 16:26:31 -0700

    Added unit test for service

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -6,6 +6,24 @@ describe('service', function() {
   beforeEach(module('myApp.services'));
 
   describe('when searching twitter', function() {
-  	it('should send the search query to twitter using $http')	
+    beforeEach(inject(function($httpBackend, twitter) {
+      $httpBackend.expectJSONP('http://search.twitter.com/search.json?callback=JSON_CALLBACK&q=thing')
+      .respond({
+        'results' : [
+          {
+          'from_user': 'user',
+          'text': 'tweet text',
+          'timestamp': new Date().toString(),
+          'profile_image_url': 'hi.jpg'
+        }
+        ]
+      });
+    }));
+
+    it('should fetch the search query from twitter', inject(function($httpBackend, twitter) {
+      var search = twitter.search('thing');
+      $httpBackend.flush();
+      expect(search[0].text).toBe('tweet text')
+    }));
   })
 });

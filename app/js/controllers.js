4d3cc0353548355e2cfc215f0f71420be0dce045
Saem Ghani <saemghani@gmail.com>
2013-05-20 18:35:08 -0700

    Better NgModelController example

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,15 +3,5 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-.controller('MainController', function($scope, $timeout, twitter) {
-  $scope.tweets = []
-
-  $scope.postTweet = function() {
-    $scope.tweets.push($scope.tweet);
-    $scope.tweet = ''
-  }
-
-  $scope.search = function() {
-    $scope.tweets = twitter.search($scope.query);
-  }
+.controller('MainController', function($scope) {
 });
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,4 +3,25 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [])
+.directive('noBro', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attr, ngModel) {
+      var brocabulary = ['bro', 'brogrammer', 'mad game', 'mongodb'];
+
+      //For DOM -> model validation
+      ngModel.$parsers.unshift(function(value) {
+        var valid = brocabulary.indexOf(value.toLowerCase()) === -1;
+        ngModel.$setValidity('brocabulary', valid);
+        return valid ? value : undefined;
+      });
+
+      //For model -> DOM validation
+      ngModel.$formatters.unshift(function(value) {
+        ngModel.$setValidity('brocabulary', brocabulary.indexOf(value.toLowerCase()) === -1);
+        return value;
+      });
+    }
+  };
+});
